0bae387f2e5f5bb22a4bda07f4fbfdb3389b0a2d
Nicolas Siver <nicolas@siver.im>
2015-05-11 13:52:55 +0300

    include client js

--- public/js/likes.js
+++ public/js/likes.js
@@ -1,9 +1,35 @@
 $(document).ready(function () {
     'use strict';
 
-    $(window).on('action:ajaxify.contentLoaded', function(e, data) {
-        if (data.tpl === 'topic') {
-            console.log('[Likes] content loaded', data);
+    require([
+        'translator'
+    ], function (translator) {
+
+        //$(window).on('action:posts.loaded', function (e, data) {
+        //    data.posts.forEach(function (post, index) {
+        //        drawRank($('[component="post/points"][data-pid="' + post.pid + '"]'), getSettings());
+        //    });
+        //});
+
+        $(window).on('action:topic.loading', function (e) {
+            addListeners();
+        });
+
+        function addListeners(){
+            $('[component="ns/likes/toggle"]').on('click', function (e) {
+                console.log('click');
+            });
+
+            $('[component="ns/likes/vote-count"]').on('click', function (e) {
+                console.log('votes');
+            });
         }
+
     });
+
+    //$(window).on('action:ajaxify.contentLoaded', function(e, data) {
+    //    if (data.tpl === 'topic') {
+    //        console.log('[Likes] content loaded', data);
+    //    }
+    //});
 });
